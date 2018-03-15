5a6f17e47e303c5fb61181c1ba3489a3d0a1224f
Lalli Nuorteva <lnuorteva@zalando-60282.corp.ad.zalando.net>
2018-03-15 14:59:40 +0200

    Install

---
+++ install.sh
@@ -0,0 +1,17 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew tap caskroom/versions
+brew cask install java8
+brew cask install visual-studio-code
+brew install ack
+brew install fzf
+# To install useful key bindings and fuzzy completion:
+$(brew --prefix)/opt/fzf/install
+
+brew cask install spotify
+brew cask install iterm2
+
+
+defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain InitialKeyRepeat -int 10
+defaults write com.apple.dock autohide-time-modifier -int 0;killall Dock
+
